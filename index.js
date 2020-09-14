4737d985503538beec55f4ff479779c0d84f9974
Cameron Clough <cameronjclough@gmail.com>
2020-09-14 01:00:03 +0100

    feat: implement Client Credentials Flow

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('Hello, world!')
+// Load dotenv config into environment
+// https://www.npmjs.com/package/dotenv
+require('dotenv').config()
+const { client_id, client_secret } = process.env
+
+const axios = require('axios')
+
+const encodeParams = (params) =>
+  Object.entries(params)
+    .map(([key, val]) => `${key}=${encodeURIComponent(val)}`)
+    .join('&')
+
+const getToken = () => {
+  return axios.post(
+    'https://accounts.spotify.com/api/token',
+    encodeParams({
+      grant_type: 'client_credentials',
+    }),
+    {
+      auth: {
+        username: client_id,
+        password: client_secret,
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    },
+  ).then((response) => {
+    return response.data.access_token
+  })
+}
+
+const main = async () => {
+  const token = await getToken()
+  console.log('Token:', token)
+}
+
+main();
